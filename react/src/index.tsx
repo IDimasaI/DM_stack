16e1be30ab788c8c0e49d4e8c7546187024c7413
IDimasaI <137656810+IDimasaI@users.noreply.github.com>
2024-02-29 13:42:04 +0300

    relice

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,21 +1,13 @@
 import React,{lazy} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-const Constellation_system = lazy(() => import('./router/App').then(module => ({default: module.Constellation_system})));
-const Regions = lazy(() => import('./router/App').then(module => ({default: module.Region_all})));
-if(document.getElementById('Constellation&System') as HTMLElement){
-  const root = ReactDOM.createRoot(document.getElementById('Constellation&System') as HTMLElement);
+const Home = lazy(() => import('./router/App').then(module => ({default: module.Home})));
+
+if(document.getElementById('Home') as HTMLElement){
+  const root = ReactDOM.createRoot(document.getElementById('Home') as HTMLElement);
   root.render(
     <React.StrictMode>
-      <Constellation_system />
-    </React.StrictMode>
-  );
-}
-if(document.getElementById('Regions') as HTMLElement){
-  const root = ReactDOM.createRoot(document.getElementById('Regions') as HTMLElement);
-  root.render(
-    <React.StrictMode>
-      <Regions />
+      <Home />
     </React.StrictMode>
   );
 }
